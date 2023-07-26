caefa059415cc06a11548c375233460d6dcee296
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-07-26 14:45:43 -0500

    Básico de Programación con C# (Ejercicio circulo)

---
+++ BasicoC#/Circulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var radio = 0d;
+            var resultado = 0d;
+            const double Pi = 3.14;
+            Console.WriteLine("Bienvenido, calculemos círculos!");
+            //Pi*radio*radio
+            Console.WriteLine("Ingrese el valor del radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            resultado = Pi * radio * radio;
+            Console.WriteLine($"El área de tu circulo es de: {resultado}");
+
+        }
+    }
+}
