adfd50475036818bccc3aef9140bf4614e408804
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-07-26 14:48:17 -0500

    Ejercicio Operadores Lógicos

---
+++ BasicoC#/OperadoresLogicos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var (valor1, valor2, valor3) = (true, true, false);
+
+            bool resultadoAnd = valor1 && valor2 && valor3;
+            Console.WriteLine(resultadoAnd);
+
+            bool resultadoOr = valor1 || valor2 || valor3;
+            Console.WriteLine(resultadoOr);
+
+            bool resultadoAndOr = (valor1 && valor2) || valor3;
+            Console.WriteLine(resultadoAndOr);
+
+            bool resultadoNot = !valor1;
+            Console.WriteLine(resultadoNot);
+
+            bool resultadoXor = valor1 ^ valor2 ^ valor3;
+            Console.WriteLine(resultadoXor);
+        }
+    }
+}
