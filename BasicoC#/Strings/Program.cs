69a4d8dec466463f89c725694ef6e152cb6e86ab
Yesenia Quejada Rojas <57049141+yeseniaquejada@users.noreply.github.com>
2023-07-26 14:48:57 -0500

    Ejercicios de Strings

---
+++ BasicoC#/Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ricardo perez ID: ");
+            int altura = 168;
+            int edad = 89;
+            string nombre = "Ricardo Perez";
+            string informacion = "Nacio en Hidalgo, es estudiante de gastronomia";
+            var hobby = "Deportista";
+
+            string tarjetaIdentificacion = $"La informacion de {nombre}, es la siguiente: \n" +
+                $"Su edad es: {edad} \nSu altura es: {altura} cm \nInformación relevante: {informacion} \n" +
+                $"Su hobby es: {hobby}";
+
+            Console.WriteLine(tarjetaIdentificacion);
+        }
+    }
+}
